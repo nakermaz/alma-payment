0fda3b978f7066aab3a10e774f0dcff017cc4e1d
nakermaz <telvintamer@gmail.com>
2026-04-02 14:01:12 +0500

    Добавил настроки для insomnia

---
+++ src/main/java/alma/alma_payment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package alma.alma_payment.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleValidationExceptions(
+            ResponseStatusException ex) {
+        return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage());
+    }
+}
